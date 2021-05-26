a496fa97ec8c58014077fc7f63c40efe0bcb0003
AttaouiAhmedAli <attaouiahmedali@gmail.com>
2021-05-26 17:08:37 +0200

    changes to controllers

--- School-management-system/src/ma/SchoolManagement/controllers/ChoixServiceController.java
+++ School-management-system/src/ma/SchoolManagement/controllers/ChoixServiceController.java
@@ -33,4 +33,39 @@ public class ChoixServiceController  implements Initializable {
 		stage.setScene(new Scene(home));
 		stage.show();
 	}
+	
+	@FXML
+	private javafx.scene.control.TextField idEtudiant;
+	
+	@FXML
+	private javafx.scene.control.RadioButton ouiBourse;
+	
+	@FXML
+	private javafx.scene.control.RadioButton nonBourse;
+	
+	@FXML
+	private javafx.scene.control.RadioButton ouiCiteUniv;
+	
+	@FXML
+	private javafx.scene.control.RadioButton nonCiteUniv;
+	
+	@FXML
+	private javafx.scene.control.RadioButton ouiCouvMed;
+	
+	@FXML
+	private javafx.scene.control.RadioButton nonCouvMed;
+	
+	@FXML
+	private javafx.scene.control.TextField couvMed;
+	
+	private void edit_couvMed() throws IOException {
+		String id = idEtudiant.getText();
+		boolean ouiB = ouiBourse.isSelected();
+		boolean nonB = nonBourse.isSelected();
+		boolean ouiCU = ouiCiteUniv.isSelected();
+		boolean nonCU = nonCiteUniv.isSelected();
+		boolean ouiCM = ouiCouvMed.isSelected();
+		boolean nonCM = nonCouvMed.isSelected();
+		String CM = couvMed.getText();
+	}
 }
--- School-management-system/src/ma/SchoolManagement/controllers/controllerEleve.java
+++ School-management-system/src/ma/SchoolManagement/controllers/controllerEleve.java
@@ -2,6 +2,7 @@ package ma.SchoolManagement.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -10,9 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import ma.SchoolManagement.Main;
 import ma.SchoolManagement.helpers.DynamicViews;
 
 public class controllerEleve  implements Initializable {
@@ -44,4 +43,130 @@ public class controllerEleve  implements Initializable {
 		stage.setScene(new Scene(home));
 		stage.show();
 	}
+	
+	@FXML
+	private javafx.scene.control.TextField nomEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField prenomEleve;
+	
+	@FXML
+	private javafx.scene.control.RadioButton sexeHomme;
+	
+	@FXML
+	private javafx.scene.control.RadioButton sexeFemme;
+	
+	@FXML
+	private javafx.scene.control.DatePicker dateNaissanceEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField adresseEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField villeEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField codePostalEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField nationnaliteEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField telephoneEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField mailEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField situationFamilialeEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField RIBEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField CNIPere;
+	
+	@FXML
+	private javafx.scene.control.TextField nomPere;
+	
+	@FXML
+	private javafx.scene.control.TextField prenomPere;
+	
+	@FXML
+	private javafx.scene.control.DatePicker dateNaissancePere;
+	
+	@FXML
+	private javafx.scene.control.DatePicker dateDecesPere;
+	
+	@FXML
+	private javafx.scene.control.TextField CNIMere;
+	
+	@FXML
+	private javafx.scene.control.TextField nomMere;
+	
+	@FXML
+	private javafx.scene.control.TextField prenomMere;
+	
+	@FXML
+	private javafx.scene.control.DatePicker dateNaissanceMere;
+	
+	@FXML
+	private javafx.scene.control.DatePicker dateDecesMere;
+	
+	@FXML
+	private javafx.scene.control.TextField CNEEleve;
+	
+	@FXML
+	private javafx.scene.control.TextField departementEleve;
+	
+	
+	
+	@FXML
+	private void add_eleve() throws IOException{
+		String nom = nomEleve.getText();
+		String prenom = prenomEleve.getText();
+		boolean homme = sexeHomme.isSelected();
+		boolean femme = sexeFemme.isSelected();
+		LocalDate ddnEleveLocal = dateNaissanceEleve.getValue();
+		String adresse = adresseEleve.getText();
+		String ville = villeEleve.getText();
+		String codePostal = codePostalEleve.getText();
+		String nationnalite = nationnaliteEleve.getText();
+		String tel = telephoneEleve.getText();
+		String mail = mailEleve.getText();
+		String sFEleve = situationFamilialeEleve.getText();
+		String RIB = RIBEleve.getText();
+		String CNE = CNEEleve.getText();
+		String dpt = departementEleve.getText();
+		String CNIP = CNIPere.getText();
+		String nomP = nomPere.getText();
+		String prenomP = prenomPere.getText();
+		LocalDate ddnPereLocal = dateNaissancePere.getValue();
+		LocalDate dddPereLocal = dateDecesPere.getValue();
+		String CNIM = CNIMere.getText();
+		String nomM = nomMere.getText();
+		String prenomM = prenomMere.getText();
+		LocalDate ddnMereLocal = dateNaissancePere.getValue();
+		LocalDate dddMereLocal = dateDecesPere.getValue();
+	}
+	
+	@FXML
+	private javafx.scene.control.CheckBox pereDeces;
+	
+	@FXML
+	private javafx.scene.control.CheckBox mereDeces;
+	
+	
+	@FXML
+	private void edit_eleve() throws IOException{
+		String adresse = adresseEleve.getText();
+		String tel = telephoneEleve.getText();
+		String mail = mailEleve.getText();
+		String sF = situationFamilialeEleve.getText();
+		String RIB = RIBEleve.getText();
+		boolean pere = pereDeces.isSelected();
+		boolean mere = mereDeces.isSelected();
+		LocalDate ddd = dateDecesMere.getValue();
+	}
+	
 }
