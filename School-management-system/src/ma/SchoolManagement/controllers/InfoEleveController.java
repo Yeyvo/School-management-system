4eedbb243d1c7c05cc007b3a606bb81530908f2a
AttaouiAhmedAli <attaouiahmedali@gmail.com>
2021-06-02 22:06:53 +0200

    changes to controllers

---
+++ School-management-system/src/ma/SchoolManagement/controllers/InfoEleveController.java
@@ -0,0 +1,78 @@
+package ma.SchoolManagement.controllers;
+
+import javafx.fxml.FXML;
+
+public class InfoEleveController {
+
+	@FXML
+	private javafx.scene.text.Text nomEleveText;
+
+	@FXML
+	private javafx.scene.text.Text prenomEleveText;
+
+	@FXML
+	private javafx.scene.text.Text sexeText;
+
+	@FXML
+	private javafx.scene.text.Text dateNaissanceEleveText;
+
+	@FXML
+	private javafx.scene.text.Text adresseEleveText;
+
+	@FXML
+	private javafx.scene.text.Text villeEleveText;
+
+	@FXML
+	private javafx.scene.text.Text codePostalEleveText;
+
+	@FXML
+	private javafx.scene.text.Text nationnaliteEleveText;
+
+	@FXML
+	private javafx.scene.text.Text telephoneEleveText;
+
+	@FXML
+	private javafx.scene.text.Text mailEleveText;
+
+	@FXML
+	private javafx.scene.text.Text situationFamilialeEleveText;
+
+	@FXML
+	private javafx.scene.text.Text RIBEleveText;
+
+	@FXML
+	private javafx.scene.text.Text CNIPereText;
+
+	@FXML
+	private javafx.scene.text.Text nomPereText;
+
+	@FXML
+	private javafx.scene.text.Text prenomPereText;
+
+	@FXML
+	private javafx.scene.text.Text dateNaissancePereText;
+
+	@FXML
+	private javafx.scene.text.Text dateDecesPereText;
+
+	@FXML
+	private javafx.scene.text.Text CNIMereText;
+
+	@FXML
+	private javafx.scene.text.Text nomMereText;
+
+	@FXML
+	private javafx.scene.text.Text prenomMereText;
+
+	@FXML
+	private javafx.scene.text.Text dateNaissanceMereText;
+
+	@FXML
+	private javafx.scene.text.Text dateDecesMereText;
+
+	@FXML
+	private javafx.scene.text.Text CNEEleveText;
+
+	@FXML
+	private javafx.scene.text.Text departementEleveText;
+}
